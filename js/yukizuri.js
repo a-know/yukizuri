5249887baaee48fb0c38aedb86546186a7c3985f
a-know <a.know.3373@gmail.com>
2017-12-10 17:37:37 +0900

    Separate js logic

---
+++ js/yukizuri.js
@@ -0,0 +1,126 @@
+$(function(){
+    var socket = null;
+    var msgBox = $("#chatbox #message");
+    var messages = $("#chat-box-messages");
+    $("#chatbox").submit(function(){
+        if (!msgBox.val()) return false;
+        if (!socket) {
+        alert("Error : Not established WebSocket connection.");
+        return false;
+        }
+        socket.send(JSON.stringify({"Message": msgBox.val()}));
+        msgBox.val("");
+        return false;
+    });
+    if (!window["WebSocket"]) {
+        alert("Error : This browser not supports WebSocket.");
+    } else {
+        socket = new WebSocket("ws://" + location.host + "/room");
+        socket.onclose = function() {
+            var icon = "./images/yukizuri-sys.png";
+            var name = "Yukizuri-sys";
+            messages.prepend(
+                $("<div>").attr("class", "chat-box-msg left")
+                .append(
+                    $("<div>").attr("class", "chat-box-danger clearfix")
+                    .append(
+                        $("<span>").attr("class", "chat-box-name pull-left").text(name))
+                    .append(
+                        $("<span>").attr("class", "chat-box-timestamp pull-right").text((new Date()))
+                    )
+                )
+                .append(
+                    $("<img>").attr("class", "chat-box-img").attr("src", icon)
+                )
+                .append(
+                    $("<div>").attr("class", "chat-box-text").text("Connection closed. Please reload this page.")
+                )
+            );
+        }
+        socket.onmessage = function(e) {
+            var msg = JSON.parse(e.data);
+            if (msg.Name == "{{.UserData.name}}") {
+                messages.prepend(
+                    $("<div>").attr("class", "chat-box-msg right")
+                    .append(
+                        $("<div>").attr("class", "chat-box-danger clearfix")
+                        .append(
+                            $("<span>").attr("class", "chat-box-name pull-right").text(msg.Name + " (" + msg.RemoteAddr + ")"))
+                        .append(
+                            $("<span>").attr("class", "chat-box-timestamp pull-left").text((new Date(msg.When)))
+                        )
+                    )
+                    .append(
+                        $("<img>").attr("class", "chat-box-img").attr("src", get_identicon(msg.Name))
+                    )
+                    .append(
+                        $("<div>").attr("class", "chat-box-text").text(msg.Message)
+                    )
+                );
+            } else {
+                var icon = "";
+                var name = "";
+                if (msg.Name == "Yukizuri-sys") {
+                    icon = "./images/yukizuri-sys.png";
+                    name = "Yukizuri-sys";
+                } else {
+                    icon = get_identicon(msg.Name);
+                    name = msg.Name + " (" + msg.RemoteAddr + ")";
+                }
+                messages.prepend(
+                    $("<div>").attr("class", "chat-box-msg left")
+                    .append(
+                        $("<div>").attr("class", "chat-box-danger clearfix")
+                        .append(
+                            $("<span>").attr("class", "chat-box-name pull-left").text(name))
+                        .append(
+                            $("<span>").attr("class", "chat-box-timestamp pull-right").text((new Date(msg.When)))
+                        )
+                    )
+                    .append(
+                        $("<img>").attr("class", "chat-box-img").attr("src", icon)
+                    )
+                    .append(
+                        $("<div>").attr("class", "chat-box-text").text(msg.Message)
+                    )
+                );
+                // refresh members list
+                $("#members-count").text(msg.CurrentMembers.length);
+                $("#members-box").empty();
+                for(let i = 0; i < msg.CurrentMembers.length; i++) {
+                    var memberName = "";
+                    if ("{{.UserData.name}}" == msg.CurrentMembers[i]["name"]) {
+                        memberName = msg.CurrentMembers[i]["name"] + " (You)";
+                    } else {
+                        memberName = msg.CurrentMembers[i]["name"];
+                    }
+                    $("#members-box")
+                    .append(
+                        $("<li>")
+                        .append(
+                            $("<img>").attr("src", get_identicon(msg.CurrentMembers[i]["name"]))
+                        )
+                        .append(
+                            $("<span>").attr("class", "users-list-date").text(memberName)
+                        )
+                        .append(
+                            $("<span>").attr("class", "users-list-date").text(msg.CurrentMembers[i]["remote_addr"])
+                        )
+                    );
+                }
+            }
+        }
+    }
+});
+
+function get_identicon(text) {
+    var salt = 0;
+    var rounds = 1;
+    var size = 210;
+    var outputType = "HEX";
+    var hashtype = "SHA-512";
+    var shaObj = new jsSHA(text+salt, "TEXT");
+    var hash = shaObj.getHash(hashtype, outputType,rounds);
+    var data = new Identicon(hash, size).toString();
+    return 'data:image/png;base64,' + data;
+}
