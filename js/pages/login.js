b2c828f20e82eafce373a17733a901df12bbd3c3
a-know <a.know.3373@gmail.com>
2017-12-30 15:52:14 +0900

    Add nickname validation

--- js/pages/login.js
+++ js/pages/login.js
@@ -1,4 +1,11 @@
 function join(){
+    input_nickname = $('#nickname').val()
+    if(input_nickname.match(/[@#%&'"\(\)=\~\|`{}\[\]\*\+\.<>\\`\?;:\/]/)) {
+        $("#join-alert").text("Symbols other than hyphens and underscores can not be used.");
+        return;
+    } else {
+        $("#join-alert").text("");
+    }
     if ($('#nickname').val() != ""){
         location.href = location.protocol + "//" + location.host + "/join/" + $('#nickname').val();
     } else {
