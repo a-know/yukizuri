9743b2d13bce7c34e402c18419b4e5dbabc7a096
ryosms <ryo.sms@gmail.com>
2020-05-19 22:57:57 +0900

    Fix building image tag
    * fix base64 regex string
    * fix replacing img tag

--- js/yukizuri.js
+++ js/yukizuri.js
@@ -1,7 +1,7 @@
 $(function(){
     var socket = null;
     var exp = /(\b(https?|ftp|file):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
-    var imageExp = /image:(.+)/;
+    var imageExp = /data:image\/(.+)/;
     var msgBox = $("#chatbox #message");
     var messages = $("#chat-box-messages");
     $("#chatbox").submit(function(){
@@ -56,7 +56,7 @@ $(function(){
                         $("<img>").attr("class", "chat-box-img").attr("src", get_identicon(msg.Name))
                     )
                     .append(
-                        $("<div>").attr("class", "chat-box-text").html(msg.Message.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'').replace(exp,"<a href='$1'>$1</a>").replace(imageExp,"<img src='$1' />"))
+                        $("<div>").attr("class", "chat-box-text").html(msg.Message.replace(/<("[^"]*"|'[^']*'|[^'">])*>/g,'').replace(exp,"<a href='$1'>$1</a>").replace(imageExp,"<img src='data:image/$1' />"))
                     )
                 );
             } else {
